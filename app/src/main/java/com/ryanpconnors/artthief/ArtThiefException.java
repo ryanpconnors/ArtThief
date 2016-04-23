b6eee80ecdd5d15cb52e54facd53954552c7bad7
Ryan Connors <ryanpconnors@gmail.com>
2016-04-23 17:51:46 -0400

    Adds star buttons to the CompareArWorkFragment onCreateView method.

---
+++ app/src/main/java/com/ryanpconnors/artthief/ArtThiefException.java
@@ -0,0 +1,15 @@
+package com.ryanpconnors.artthief;
+
+/**
+ * Created by Ryan Connors on 4/23/16.
+ */
+public class ArtThiefException extends Exception {
+
+    public ArtThiefException() {
+        super();
+    }
+
+    public ArtThiefException(String message) {
+        super(message);
+    }
+}
--- app/src/main/java/com/ryanpconnors/artthief/compare/CompareArtWorkFragment.java
+++ app/src/main/java/com/ryanpconnors/artthief/compare/CompareArtWorkFragment.java
@@ -8,9 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.ryanpconnors.artthief.R;
 import com.ryanpconnors.artthief.artgallery.Gallery;
+
+import java.util.logging.Logger;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,13 +26,12 @@ import com.ryanpconnors.artthief.artgallery.Gallery;
 public class CompareArtWorkFragment extends Fragment {
 
     private static final String TAG = "CompareArtWorkFragment";
-
+    private static final Logger LOG = Logger.getLogger(CompareArtWorkFragment.TAG);
     private Button mFiveStarButton;
     private Button mFourStarButton;
     private Button mThreeStarButton;
     private Button mTwoStarButton;
     private Button mOneStarButton;
-
     private OnCompareArtFragmentInteractionListener mCompareArtWorkListener;
 
     public CompareArtWorkFragment() {
@@ -57,7 +59,7 @@ public class CompareArtWorkFragment extends Fragment {
         // Retain this fragment to ensure that the asynchronous UpdateArtWorksTask completes
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            // store given arguments in this class instance
+            //TODO: store given arguments in this class instance
         }
     }
 
@@ -65,34 +67,124 @@ public class CompareArtWorkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_compare_art, container, false);
 
-        // Setup the compare artwork star buttons
-        mFiveStarButton = (Button) v.findViewById(R.id.compare_artwork_five_star_button);
-        int fiveStarCount = Gallery.get(getActivity()).getStarCount(getString(R.string.five_stars));
-        String fiveStarButtonText = getResources()
-                .getString(R.string.compare_five_star_button_label, fiveStarCount);
-        mFiveStarButton.setText(fiveStarButtonText);
-
-        if (fiveStarCount > 0) {
-            mFiveStarButton.setActivated(true);
-            mFiveStarButton.setClickable(true);
-        } else {
-            mFiveStarButton.setActivated(false);
-            mFiveStarButton.setClickable(false);
-        }
-        mFiveStarButton.setOnClickListener(new View.OnClickListener() {
+        mOneStarButton = (Button) v.findViewById(R.id.compare_artwork_one_star_button);
+        mOneStarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO setup onClick for star buttons
+                Toast.makeText(getActivity().getApplicationContext(), "One Star:" +
+                                Gallery.get(getActivity()).getStarCount(getString(R.string.one_star)),
+                        Toast.LENGTH_LONG).show();
             }
         });
 
-        //TODO setup other star buttons
+        mTwoStarButton = (Button) v.findViewById(R.id.compare_artwork_two_star_button);
+        mTwoStarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Two Star:" +
+                                Gallery.get(getActivity()).getStarCount(getString(R.string.two_stars)),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
+        mThreeStarButton = (Button) v.findViewById(R.id.compare_artwork_three_star_button);
+        mThreeStarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Three Star:" +
+                                Gallery.get(getActivity()).getStarCount(getString(R.string.three_stars)),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
+        mFourStarButton = (Button) v.findViewById(R.id.compare_artwork_four_star_button);
+        mFourStarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Four Star:" +
+                                Gallery.get(getActivity()).getStarCount(getString(R.string.four_stars)),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mFiveStarButton = (Button) v.findViewById(R.id.compare_artwork_five_star_button);
+        mFiveStarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Five Star:" +
+                                Gallery.get(getActivity()).getStarCount(getString(R.string.five_stars)),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        refreshStarButtons();
         return v;
+    }
+
+    private void refreshStarButtons() {
+        for (int i = 1; i <= 5; i++) {
+            setStarButtonStatus(i);
+        }
+    }
+
+    private void setStarButtonStatus(int numberOfStars) {
+
+        Button starButton;
+        String starButtonText;
+        int starCount;
+
+        switch (numberOfStars) {
+            case 1:
+                starButton = mOneStarButton;
+                starCount = Gallery.get(getActivity()).getStarCount(getString(R.string.one_star));
+                starButtonText = getResources()
+                        .getString(R.string.compare_one_star_button_label, starCount);
+                break;
+
+            case 2:
+                starButton = mTwoStarButton;
+                starCount = Gallery.get(getActivity()).getStarCount(getString(R.string.two_stars));
+                starButtonText = getResources()
+                        .getString(R.string.compare_two_star_button_label, starCount);
+                break;
+
+            case 3:
+                starButton = mThreeStarButton;
+                starCount = Gallery.get(getActivity()).getStarCount(getString(R.string.three_stars));
+                starButtonText = getResources()
+                        .getString(R.string.compare_three_star_button_label, starCount);
+                break;
+
+            case 4:
+                starButton = mFourStarButton;
+                starCount = Gallery.get(getActivity()).getStarCount(getString(R.string.four_stars));
+                starButtonText = getResources()
+                        .getString(R.string.compare_four_star_button_label, starCount);
+                break;
+
+            case 5:
+                starButton = mFiveStarButton;
+                starCount = Gallery.get(getActivity()).getStarCount(getString(R.string.five_stars));
+                starButtonText = getResources()
+                        .getString(R.string.compare_five_star_button_label, starCount);
+                break;
+
+            default:
+                LOG.warning(TAG + ": Invalid number of stars: " + numberOfStars);
+                return;
+        }
+
+        starButton.setText(starButtonText);
+
+        if (starCount > 0) {
+            starButton.setEnabled(true);
+            starButton.setAlpha(1f);
+        } else {
+            starButton.setEnabled(starCount > 0);
+            starButton.setAlpha(.5f);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
