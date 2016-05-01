1cb2e0abff273a0838f2f2408115139ea3b17b73
Ryan Connors <ryanpconnors@gmail.com>
2016-05-01 11:52:41 -0400

    Adds class fields and refactors SortArtWorkFragment class.

--- app/src/main/java/com/ryanpconnors/artthief/compare/SortArtWorkFragment.java
+++ app/src/main/java/com/ryanpconnors/artthief/compare/SortArtWorkFragment.java
@@ -9,45 +9,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ryanpconnors.artthief.R;
+import com.ryanpconnors.artthief.artgallery.ArtWork;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SortArtWorkFragment.OnFragmentInteractionListener} interface
+ * {@link SortArtWorkFragment.OnSortArtworkFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link SortArtWorkFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SortArtWorkFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_NUMBER_OF_STARS = "number_of_stars";
 
-    private OnFragmentInteractionListener mListener;
+    private int mNumberOfStars;
+
+    private List<ArtWork> mArtWorks;
+
+    private OnSortArtworkFragmentInteractionListener mListener;
 
     public SortArtWorkFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SortArtWorkFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SortArtWorkFragment newInstance(String param1, String param2) {
+    public static SortArtWorkFragment newInstance(int numberOfStars) {
         SortArtWorkFragment fragment = new SortArtWorkFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_NUMBER_OF_STARS, numberOfStars);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +47,9 @@ public class SortArtWorkFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mNumberOfStars = getArguments().getInt(ARG_NUMBER_OF_STARS);
+            // TODO: get all the artworks with the number of stars given that also have photos available
+            setHasOptionsMenu(true);
         }
     }
 
@@ -78,8 +70,8 @@ public class SortArtWorkFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnSortArtworkFragmentInteractionListener) {
+            mListener = (OnSortArtworkFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -102,7 +94,7 @@ public class SortArtWorkFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnSortArtworkFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
